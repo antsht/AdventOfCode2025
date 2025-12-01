9ced0e6024c6e6de92e2a0ca36a5e671bc8de6cb
Anton Shtern <antsht@gmail.com>
2025-12-01 18:31:33 +0300

    Done: day 1_2

--- src/Day_1_2/Program.cs
+++ src/Day_1_2/Program.cs
@@ -1,5 +1,22 @@
-﻿// read input.txt
+﻿// if TEST is defined, read input.txt
+//#define TEST
+#if TEST
+// read input.txt
 string[] input = File.ReadAllLines("input.txt");
+#else
+// get input from https://adventofcode.com/2025/day/1/input
+// add cookie to the request
+HttpClient client = new HttpClient();
+client.DefaultRequestHeaders.Add("Cookie", "session=MYSECRETSESSIONCOOKIE;");
+string inputRaw = await client.GetStringAsync("https://adventofcode.com/2025/day/1/input");
+Console.WriteLine(inputRaw);
+// to array of lines except the last line
+string[] input = inputRaw.Split('\n')[..^1];
+#endif
+
+
+
+
 // parse input L00 or R00
 var parsedInput = input.Select(line => line[0]=='L' ? -int.Parse(line[1..]) : int.Parse(line[1..])).ToList();
 
@@ -8,10 +25,13 @@ int startPosition = 50;
 int currentPosition = startPosition;
 int zeroes = 0;
 foreach (var number in parsedInput) {
-    if ((currentPosition!= 0 && currentPosition + number < 0) || currentPosition + number > 100) {
+    zeroes+= Math.Abs(number) / 100;
+    int number_normalized = number % 100;
+    if ((currentPosition!= 0 && currentPosition + number_normalized < 0) || currentPosition + number_normalized > 100) {
         zeroes++;
     }
-    currentPosition = ((currentPosition + number)<0 ? 100+(currentPosition + number):(currentPosition+number)) % 100;
+    
+    currentPosition = ((currentPosition + number_normalized)<0 ? 100+(currentPosition + number_normalized):(currentPosition+number_normalized)) % 100;
     if (currentPosition == 0) {
         zeroes++;
     }
