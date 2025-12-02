e1a1edf2e80b1a77476847e371c2c27c2e30fd4e
Anton Shtern <antsht@gmail.com>
2025-12-02 09:14:32 +0300

    Done: Day 2_1

---
+++ src/Day_2_1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+string line = File.ReadLines("input.txt").First();
+string[] ranges = line.Split(',');
+long total = 0;
+foreach (var range in ranges) {
+     (long rangeStart, long rangeEnd) = (long.Parse(range.Split('-')[0]), long.Parse(range.Split('-')[1]));
+     for (long i = rangeStart; i <= rangeEnd; i++) {
+        total += CheckValue(i.ToString())? i : 0;
+     }
+}
+Console.WriteLine(total);
+
+bool CheckValue(string value) {
+    int digits = value.Length;
+    if (digits % 2 == 1) {
+        return false;
+    }
+    if (value[0..(digits/2)] != value[(digits/2)..digits]) {
+        return false;
+    }
+    return true;
+}
