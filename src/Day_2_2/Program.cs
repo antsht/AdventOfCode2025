0b2c1ae0dac7fe360239a96a31cbd9777ff2116c
Anton Shtern <antsht@gmail.com>
2025-12-02 09:21:15 +0300

    Done: Day 2_2

---
+++ src/Day_2_2/Program.cs
@@ -0,0 +1,37 @@
+﻿
+string line = File.ReadLines("input.txt").First();
+string[] ranges = line.Split(',');
+long total = 0;
+foreach (var range in ranges) {
+     (long rangeStart, long rangeEnd) = (long.Parse(range.Split('-')[0]), long.Parse(range.Split('-')[1]));
+     for (long i = rangeStart; i <= rangeEnd; i++) {
+        total += CheckValue(i.ToString())? i : 0;
+     }
+}
+Console.WriteLine(total);
+
+bool CheckValue(string value) {
+    int length = value.Length;
+    
+    for (int patternLength = 1; patternLength <= length / 2; patternLength++) {
+        if (length % patternLength == 0) {
+            int repetitions = length / patternLength;
+            if (repetitions >= 2) {
+                string pattern = value[0..patternLength];
+                bool isMatch = true;
+                
+                for (int i = 0; i < length; i++) {
+                    if (value[i] != pattern[i % patternLength]) {
+                        isMatch = false;
+                        break;
+                    }
+                }
+                if (isMatch) {
+                    return true;
+                }
+            }
+        }
+    }
+    
+    return false;
+}
