68239a09af89060e2bfe3d1ec6f4d5f34aa72119
robynarl <antsht@gmail.com>
2025-12-03 12:28:22 +0200

    Done: Day 3

---
+++ src/Day_3_1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+string[] lines = File.ReadAllLines("input.txt");
+
+int sum = 0;
+
+foreach (string line in lines)
+{
+    int[] digits = line.Select(c => int.Parse(c.ToString())).ToArray();
+    
+    int maxJoltage = 0;
+    
+    // Try all possible pairs of positions, maintaining order
+    for (int i = 0; i < digits.Length; i++)
+    {
+        for (int j = i + 1; j < digits.Length; j++)
+        {
+            // Only use the order as they appear in the string (i before j)
+            int joltage = digits[i] * 10 + digits[j];
+            maxJoltage = Math.Max(maxJoltage, joltage);
+        }
+    }
+    
+    sum += maxJoltage;
+}
+Console.WriteLine(sum);
+
---
+++ src/Day_3_2/Program.cs
@@ -0,0 +1,39 @@
+﻿
+string[] lines = File.ReadAllLines("input.txt");
+
+long sum = 0;
+
+foreach (string line in lines)
+{
+    int[] digits = line.Select(c => int.Parse(c.ToString())).ToArray();
+    int n = digits.Length;
+    int k = 12; // number of batteries to select
+    
+    List<int> selectedIndices = new List<int>();
+    int lastIndex = -1;
+    
+    // Greedy algorithm: for each position in the result, pick the largest digit from the valid range
+    for (int pos = 0; pos < k; pos++)
+    {
+        int searchStart = lastIndex + 1;
+        int searchEnd = n - (k - pos);
+        
+        int maxIndex = searchStart;
+        for (int i = searchStart; i <= searchEnd; i++)
+        {
+            if (digits[i] > digits[maxIndex])
+            {
+                maxIndex = i;
+            }
+        }
+        
+        selectedIndices.Add(maxIndex);
+        lastIndex = maxIndex;
+    }
+    
+    string joltageStr = string.Join("", selectedIndices.Select(i => digits[i]));
+    long joltage = long.Parse(joltageStr);
+    sum += joltage;
+}
+Console.WriteLine(sum);
+
