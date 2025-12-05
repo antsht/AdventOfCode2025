bca0217b34a6a85a147006414028083582ac8afc
robynarl <antsht@gmail.com>
2025-12-05 18:43:49 +0200

    Done: Day_5

---
+++ src/Day_5_1/Program.cs
@@ -0,0 +1,22 @@
+﻿string[] lines = File.ReadAllLines("input1.txt");
+
+var ranges = new List<(long start, long end)>();
+foreach (var line in lines){
+    ranges.Add((long.Parse(line.Split('-')[0]), long.Parse(line.Split('-')[1])));
+}
+
+string[] lines2 = File.ReadAllLines("input2.txt");
+
+var ingridients = new List<long>();
+foreach (var line in lines2){
+    ingridients.Add(long.Parse(line));
+}
+
+long total = 0;
+foreach (var ingridient in ingridients){
+    // if ingridient is in any range, add 1 to total
+    if (ranges.Any(range => ingridient >= range.start && ingridient <= range.end)){
+        total++;
+    }
+}
+Console.WriteLine(total);
---
+++ src/Day_5_2/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] lines = File.ReadAllLines("input1.txt");
+
+var ranges = new List<(long start, long end)>();
+foreach (var line in lines){
+    ranges.Add((long.Parse(line.Split('-')[0]), long.Parse(line.Split('-')[1])));
+}
+
+ranges.Sort((a, b) => a.start.CompareTo(b.start));
+
+for (int i = 0; i < ranges.Count-1; i++){
+    if (ranges[i+1].start >= ranges[i].start && ranges[i+1].start <= ranges[i].end){
+        ranges[i+1] = (ranges[i].start, Math.Max(ranges[i+1].end, ranges[i].end));
+        ranges[i] = (0, 0);
+    }
+}
+
+ranges.RemoveAll(range => range.start == 0 && range.end == 0);
+
+long total = 0;
+foreach (var range in ranges)
+{
+    total += range.end - range.start + 1;
+}
+
+Console.WriteLine(total);
