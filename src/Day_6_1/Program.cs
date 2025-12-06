e80a963b4b528c6476da89b361bf06580d924466
robynarl <antsht@gmail.com>
2025-12-06 11:41:00 +0200

    Done: Day_6

---
+++ src/Day_6_1/Program.cs
@@ -0,0 +1,34 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+
+List<long> numbers1 = lines[0].Split(' ').Select(long.Parse).ToList();
+List<long> numbers2 = lines[1].Split(' ').Select(long.Parse).ToList();
+List<long> numbers3 = lines[2].Split(' ').Select(long.Parse).ToList();
+List<long> numbers4 = lines[3].Split(' ').Select(long.Parse).ToList();
+
+List<char> operations = new List<char>();
+for(int i = 0; i < lines[4].Length; i++)
+{
+    if(lines[4][i] == '+')
+    {
+        operations.Add('+');
+    }
+    else if(lines[4][i] == '*')
+    {
+        operations.Add('*');
+    }
+}
+
+long result = 0;
+for(int i = 0; i < numbers1.Count; i++)
+{
+    switch (operations[i])
+    {
+        case '+':
+            result += numbers1[i] + numbers2[i] + numbers3[i] + numbers4[i];
+            break;
+        case '*':
+            result += numbers1[i] * numbers2[i] * numbers3[i] * numbers4[i];
+            break;
+    }
+}
+Console.WriteLine(result);
---
+++ src/Day_6_2/Program.cs
@@ -0,0 +1,36 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+
+// transpose lines as 2d array of chars and save it to grid
+char[,] grid = new char[lines[0].Length, lines.Length];
+for(int i = 0; i < lines.Length; i++)
+{
+    for(int j = 0; j < lines[i].Length; j++)
+    {
+        grid[j, i] = lines[i][j];
+    }
+}
+
+
+long result = 0;
+for(int i = 0; i < grid.GetLength(0); i++)
+{
+    long subResult = 0;
+    if(grid[i, 4] == '+'){
+        while(i < grid.GetLength(0) && (grid[i, 0] != ' ' || grid[i, 1] != ' ' || grid[i, 2] != ' ' || grid[i, 3] != ' '))
+        {
+            subResult += long.Parse(grid[i, 0].ToString() + grid[i, 1].ToString() + grid[i, 2].ToString() + grid[i, 3].ToString());
+            i++;
+        }
+    }
+        if(i < grid.GetLength(0) && grid[i, 4] == '*'){
+            subResult = 1;
+        while(i < grid.GetLength(0) && (grid[i, 0] != ' ' || grid[i, 1] != ' ' || grid[i, 2] != ' ' || grid[i, 3] != ' '))
+        {
+            subResult *= long.Parse(grid[i, 0].ToString() + grid[i, 1].ToString() + grid[i, 2].ToString() + grid[i, 3].ToString());
+            i++;
+        }
+    }
+    result += subResult;
+}
+
+Console.WriteLine(result);
