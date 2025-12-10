8fa489a8007c2d1165cc1d323852c4f47f079444
robynarl <antsht@gmail.com>
2025-12-10 12:24:41 +0200

    Done: Day_9_1

---
+++ src/Day_9_1/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+
+var points = new List<(long x, long y)>();
+foreach (var line in lines)
+{
+    var parts = line.Split(',');
+    points.Add((long.Parse(parts[0]), long.Parse(parts[1])));
+}
+
+long maxArea = 0;
+foreach (var point in points)
+{
+    long area = 0;
+    foreach (var otherPoint in points)
+    {
+        area = (Math.Abs(point.x - otherPoint.x)+1) * (Math.Abs(point.y - otherPoint.y)+1);
+        maxArea = Math.Max(maxArea, area);
+    }
+}
+Console.WriteLine(maxArea);
